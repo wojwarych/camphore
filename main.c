164c7d715c95539c888929d9303c15da0e95b16e
wojwarych <woj.warych@gmail.com>
2025-10-22 17:34:45 +0200

    colouring directories in different colour

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <sys/stat.h>
 
 typedef struct {
   char*  name;
@@ -25,7 +26,13 @@ int main(int argc, char* argv[]) {
       int items_size = 1;
       struct dirent* dir = readdir(d);
       while (dir != NULL) {
-        printf("%s\n", dir->d_name);
+        struct stat path_stat;
+        stat(dir->d_name, &path_stat);
+        if S_ISDIR(path_stat.st_mode) {
+          printf("\033[96m%s\033[0m\n", dir->d_name);
+        } else {
+          printf("%s\n", dir->d_name);
+        }
 
         if (items_in == items_size) {
           items_size += 1;
