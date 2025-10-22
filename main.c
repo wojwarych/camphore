129df011615ce33cc1b4abee5cbb070bb5e16bfd
wojwarych <woj.warych@gmail.com>
2025-10-22 17:21:22 +0200

    iterate over contents of dir and print to stdout

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <dirent.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+  char*  name;
+  bool   is_dir;
+} Item;
 
 
 int main(int argc, char* argv[]) {
@@ -9,11 +15,33 @@ int main(int argc, char* argv[]) {
     if (d == NULL) {
       return -1;
     } else {
+
+      Item* items = malloc(sizeof(Item));
+      if (items == NULL) {
+        return -1;
+      }
+
+      int items_in = 0;
+      int items_size = 1;
       struct dirent* dir = readdir(d);
       while (dir != NULL) {
         printf("%s\n", dir->d_name);
+
+        if (items_in == items_size) {
+          items_size += 1;
+          items_size = (int)items_size * 2;
+          items = realloc(items, (sizeof(Item) * items_size));
+          if (items == NULL) {
+            return -1;
+          }
+        }
+
+        Item item = { .name = dir->d_name, .is_dir = false };
+        items[items_in] = item;
+        items_in++;
         dir = readdir(d);
       }
+      free(items);
     }
   }
 
