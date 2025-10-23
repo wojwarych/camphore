077b62b4d64adc68fda286e6a68f6c0576f61565
wojwarych <woj.warych@gmail.com>
2025-10-23 11:23:48 +0200

    added size of the file + .clang-format support

--- main.c
+++ main.c
@@ -1,37 +1,38 @@
 #include <dirent.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <sys/stat.h>
 
 typedef struct {
-  char*  name;
-  bool   is_dir;
+  char *name;
+  bool is_dir;
 } Item;
 
-
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   if (argc == 1) {
-    DIR* d = opendir(".");
+    DIR *d = opendir(".");
     if (d == NULL) {
       return -1;
     } else {
 
-      Item* items = malloc(sizeof(Item));
+      Item *items = malloc(sizeof(Item));
       if (items == NULL) {
         return -1;
       }
 
       int items_in = 0;
       int items_size = 1;
-      struct dirent* dir = readdir(d);
+      struct dirent *dir = readdir(d);
       while (dir != NULL) {
         struct stat path_stat;
         stat(dir->d_name, &path_stat);
-        if S_ISDIR(path_stat.st_mode) {
-          printf("\033[96m%s\033[0m\n", dir->d_name);
+        if S_ISDIR (path_stat.st_mode) {
+          printf("\033[96m%s\t%jd\033[0m\n", dir->d_name,
+                 (intmax_t)path_stat.st_size);
         } else {
-          printf("%s\n", dir->d_name);
+          printf("%s\t%jd\n", dir->d_name, (intmax_t)path_stat.st_size);
         }
 
         if (items_in == items_size) {
@@ -43,7 +44,7 @@ int main(int argc, char* argv[]) {
           }
         }
 
-        Item item = { .name = dir->d_name, .is_dir = false };
+        Item item = {.name = dir->d_name, .is_dir = false};
         items[items_in] = item;
         items_in++;
         dir = readdir(d);
