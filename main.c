d2a43cd1e865d9cdab3199703299531f3c41d38e
wojwarych <woj.warych@gmail.com>
2025-10-24 09:40:51 +0200

    sort inplace by file name and print the output to terminal

--- main.c
+++ main.c
@@ -3,12 +3,29 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
+
+int get_int_len(int value) {
+  int l = 1;
+  while (value > 9) {
+    l++;
+    value /= 10;
+  }
+  return l;
+}
 
 typedef struct {
   char *name;
   bool is_dir;
 } Item;
+
+int comp(const void *a, const void *b) {
+  Item *x = (Item *)a;
+  Item *y = (Item *)b;
+
+  return (strcmp(x->name, y->name));
+}
 
 int main(int argc, char *argv[]) {
   if (argc == 1) {
@@ -26,15 +43,6 @@ int main(int argc, char *argv[]) {
       int items_size = 1;
       struct dirent *dir = readdir(d);
       while (dir != NULL) {
-        struct stat path_stat;
-        stat(dir->d_name, &path_stat);
-        if S_ISDIR (path_stat.st_mode) {
-          printf("\033[96m%s\t%jd\033[0m\n", dir->d_name,
-                 (intmax_t)path_stat.st_size);
-        } else {
-          printf("%s\t%jd\n", dir->d_name, (intmax_t)path_stat.st_size);
-        }
-
         if (items_in == items_size) {
           items_size += 1;
           items_size = (int)items_size * 2;
@@ -49,6 +57,20 @@ int main(int argc, char *argv[]) {
         items_in++;
         dir = readdir(d);
       }
+
+      qsort(items, items_in, sizeof(Item), comp);
+
+      for (int i = 0; i < items_in; i++) {
+        struct stat path_stat;
+        stat(items[i].name, &path_stat);
+        if S_ISDIR (path_stat.st_mode) {
+          printf("\033[96m%s\t%jd\033[0m\n", items[i].name,
+                 (intmax_t)path_stat.st_size);
+        } else {
+          printf("%s\t%jd\n", items[i].name, (intmax_t)path_stat.st_size);
+        }
+      }
+
       free(d);
       free(items);
     }
