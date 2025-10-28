ab273dced1cb0eb9591768d9cf39e4f663ce5f6c
wojwarych <woj.warych@gmail.com>
2025-10-28 09:32:01 +0100

    added item constructor with new fields to the struct

--- file_items.c
+++ file_items.c
@@ -7,3 +7,8 @@ int comp(const void *a, const void *b) {
 
   return (strcmp(x->name, y->name));
 }
+
+Item new_item(char *name, bool is_dir, __mode_t type, intmax_t size) {
+  Item item = {.name = name, .is_dir = is_dir, .type = type, .size = size};
+  return item;
+}
--- file_items.h
+++ file_items.h
@@ -1,12 +1,17 @@
 #ifndef FUNCTIONS_H_INCLUDED
 #define FUNCTIONS_H_INCLUDED
 #include <stdbool.h>
+#include <stdint.h>
+#include <sys/stat.h>
 
 typedef struct {
   char *name;
   bool is_dir;
+  __mode_t type;
+  intmax_t size;
 } Item;
 
 int comp(const void *a, const void *b);
+Item new_item(char *, bool, __mode_t, intmax_t);
 
 #endif
